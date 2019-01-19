7aa0daf760d2613d763701e55eaca4b80eb2d861
Yakov Kulgeyko <yakov@digitalspace.one>
2019-01-19 09:04:39 +0200

    test

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -15,7 +15,7 @@ export default class App extends Component {
                     fontSize:        24,
                     fontWeight:      '600',
                 }}>
-                Старт проекта!!!
+                Старт проекта наконец то!!!
             </h1>
         );
     }
