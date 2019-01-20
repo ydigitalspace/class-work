ec64c4f957e164d06999d9a49bb9bc8e5e91c0c5
Yakov Kulgeyko <yakov@digitalspace.one>
2019-01-20 21:32:25 +0200

    second practica

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
-
 import Post from 'components/Post';
+
+import Styles from "./styles.m.css";
 
 export default class Feed extends Component {
     render () {
         return (
-            <section>               
+            <section className = { Styles.feed }>               
+                <StatusBar />
                 <Composer />
                 <Post />
             </section>                                        
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 import avatar from 'theme/assets/lisa';
-
+import Styles from "./styles.m.css";
 import moment from 'moment';
 
 export default class Post extends Component {
     render () {
         return (
-            <section>               
+            <section className = { Styles.post } >               
                 <img src = {avatar} />
                 <a>Lisa Simpson</a>                        
                 <time>{moment().format('MMMM D Y, H:mm:ss')}</time>
