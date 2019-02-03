999b20133286f945eecf05657e4989f7faf99b34
Yakov Kulgeyko <yakov@digitalspace.one>
2019-02-03 19:49:57 +0200

    5 lesson

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -3,16 +3,34 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 import Styles from "./styles.m.css";
 
 export default class Feed extends Component {
+
+    state = {
+        posts: [
+            { id: '123', comment: 'Hello', created: 1526825076849}, 
+            { id: '125', comment: 'I am Yakov', created: 1526895016849}
+        ],
+        isSpinning: true, 
+    }
+
     render () {
+        const { posts, isSpinning } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } {...post}/>;
+        });
+
+
         return (
             <section className = { Styles.feed }>               
+                <Spinner isSpinning = { isSpinning }/>
                 <StatusBar  />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>                                        
         );
     } 
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 
 import Styles from "./styles.m.css";
-
+import PropTypes from 'prop-types';
 import { Consumer } from 'components/HOC/withProfile';
 
 import moment from 'moment';
 
 export default class Post extends Component {
+    static propTypes = {
+        comment: PropTypes.string.isRequired,
+        created: PropTypes.number.isRequired,
+    };
+
     render () {
+        const { comment, created} = this.props;
+
         return (
             <Consumer>
                 {(context) => (
                     <section className = { Styles.post } >               
                         <img src = { context.avatar } />
                         <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>                        
-                        <time>{moment().format('MMMM D Y, H:mm:ss')}</time>
-                        <p>Howdy!</p>
+                        <time>{moment.unix(created).format('MMMM D, H:mm:ss')}</time>
+                        <p>{comment}</p>
                     </section>
                 )}    
             </Consumer>                    
